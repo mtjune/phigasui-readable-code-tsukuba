1b02fc1b272e965d34eaf43311f28d60b1013adf
phigasui <phigasui@gmail.com>
2015-07-04 11:49:51 +0900

    spec6: id select

--- recipe.rb
+++ recipe.rb
@@ -2,10 +2,21 @@
 # coding: utf-8
 
 file_name = ARGV.length > 0 ? ARGV[0] : "recipe-data.txt"
+if ARGV.length > 1
+  input_id = ARGV[1]
+else
+  puts "./recipe.rb [filename] [recipe_id]"
+  exit()
+end
 
+recipes = {}
 File.open(file_name, "r") do |recipe_data|
+
   recipe_data.readlines.each do |recipe|
-    # recipe_title = recipe_line
-    puts recipe
-  end
+    recipe_id, recipe_title = recipe.split(":").map(&:strip)
+    recipes[recipe_id] = recipe_title
+    end
+
+  puts "#{input_id}: #{recipes[input_id]}"
+
 end
