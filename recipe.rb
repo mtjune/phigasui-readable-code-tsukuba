eaa6b5c043c93ab3ae4c15d18f2337654c11ae7c
Yukich <june.ymd@gmail.com>
2015-07-04 14:04:53 +0900

    レシピのID指定に対応

--- recipe.rb
+++ recipe.rb
@@ -2,6 +2,7 @@
 # coding: utf-8
 
 file_name = ARGV.length > 0 ? ARGV[0] : "recipe-data.txt"
+
 # if ARGV.length > 1
 #   input_id = ARGV[1]
 # else
@@ -14,9 +15,20 @@ File.open(file_name, "r") do |recipe_data|
 
   recipe_data.readlines.each_with_index do |recipe, recipe_id|
     recipes[recipe_id] = recipe
-    puts "#{recipe_id}: #{recipes[recipe_id]}"
+    # puts "#{recipe_id}: #{recipes[recipe_id]}"
   end
 
   # puts "#{input_id}: #{recipes[input_id]}"
 
+end
+
+
+# 表示
+if ARGV.length > 1
+  recipe_id = ARGV[1].to_i
+  puts "#{recipe_id}: #{recipes[recipe_id]}"
+else
+  recipes.each do |recipe_id, recipe_content|
+    puts "#{recipe_id}: #{recipe_content}"
+  end
 end
