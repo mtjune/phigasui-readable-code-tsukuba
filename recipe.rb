69490eaac85237134a3a745e7de7c27e723afb49
phigasui <phigasui@gmail.com>
2015-07-04 11:27:03 +0900

    spec5:

--- recipe.rb
+++ recipe.rb
@@ -4,6 +4,8 @@
 file_name = ARGV.length > 0 ? ARGV[0] : "recipe-data.txt"
 
 File.open(file_name, "r") do |recipe_data|
-  recipe_title = recipe_data.gets
-  puts recipe_title
+  recipe_data.readlines.each do |recipe|
+    # recipe_title = recipe_line
+    puts recipe
+  end
 end
