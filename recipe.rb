5cc3f1a7e50c646833c912067025cf0b29672b95
phigasui <phigasui@gmail.com>
2015-07-04 11:53:05 +0900

    spec7: add description

--- recipe.rb
+++ recipe.rb
@@ -2,12 +2,12 @@
 # coding: utf-8
 
 file_name = ARGV.length > 0 ? ARGV[0] : "recipe-data.txt"
-if ARGV.length > 1
-  input_id = ARGV[1]
-else
-  puts "./recipe.rb [filename] [recipe_id]"
-  exit()
-end
+# if ARGV.length > 1
+#   input_id = ARGV[1]
+# else
+#   puts "./recipe.rb [filename] [recipe_id]"
+#   exit()
+# end
 
 recipes = {}
 File.open(file_name, "r") do |recipe_data|
@@ -15,8 +15,9 @@ File.open(file_name, "r") do |recipe_data|
   recipe_data.readlines.each do |recipe|
     recipe_id, recipe_title = recipe.split(":").map(&:strip)
     recipes[recipe_id] = recipe_title
-    end
+    puts recipe
+  end
 
-  puts "#{input_id}: #{recipes[input_id]}"
+  # puts "#{input_id}: #{recipes[input_id]}"
 
 end
